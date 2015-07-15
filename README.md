5ed039bf349ca44dda3bff8e0e95d04f150ee42f
dklight <dklight@gmail.com>
2015-07-15 00:51:36 -0300

    Add install script Update README with installation instructions

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ dklight's dotrc files and stuff
    * fugitive - https://github.com/tpope/vim-fugitive.git
  * ZSH configuration with oh-my-zsh (http://ohmyz.sh/)
   * It requaires installation of the ad-hoc configurations
+ * GNU Screen
+
+
+Installation
+------------
+
+To install just copy this to your terminal:
+
+    curl -L https://raw.githubusercontent.com/dklight/dotrc/master/install.sh
---
+++ install.sh
@@ -0,0 +1,36 @@
+set -e
+
+# Set paths
+PROJECTS=~/projects
+DOTRC=$PROJECTS/dotrc
+
+# Download configuration
+mkdir -p $PROJECTS
+cd $PROJECTS
+git clone git@github.com:dklight/dotrc.git
+cd ~/.
+
+# Install oh-my-zsh
+if [ ! -n "$ZSH" ]; then
+  sudo apt-get install -y zsh
+  curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+  rm -f ~/.zshrc
+  ln -s $DOTRC/zshrc ~/.zshrc
+  ln -s $DOTRC/oh-my-zsh/custom/dklight.zsh ~/.oh-my-zsh/custom/
+  chsh -s $(which zsh)
+fi
+
+# Install GNU Screen
+sudo apt-get install -y screen
+ln -s %DOTRC/screenrc ~/.screenrc
+
+# Install VIM
+sudo apt-get install -y vim-gtk
+rm -f ~/.vimrc
+ln -s $DOTRC/vimrc ~/.vimrc
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle
+git clone https://github.com/bling/vim-airline
+git clone git://github.com/altercation/vim-colors-solarized.git
+git clone https://github.com/tpope/vim-fugitive.git
