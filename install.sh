a08cd10dbbbc0ecab2b7ca60e9d1f76d465b5d22
dklight <dklight@gmail.com>
2015-07-19 01:15:04 -0300

    Rename files to use bootstrap.sh in the future. Add TODO Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,27 @@
 set -e
 
 # Set paths
-PROJECTS=~/projects
+PROJECTS=`pwd`
 DOTRC=$PROJECTS/dotrc
 
 # Install git adn download configuration
 sudo apt-get install -y git
-mkdir -p $PROJECTS
-cd $PROJECTS
 git clone https://github.com:dklight/dotrc
-ln -s $DOTRC/gitconfig ~/.gitconfig
+ln -s $DOTRC/_gitconfig ~/.gitconfig
 cd ~/.
 
-# Install oh-my-zsh
+# Install zsh and antigen
 if [ ! -n "$ZSH" ]; then
   sudo apt-get install -y zsh
-  curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+  git clone https://github.com/zsh-users/antigen.git ~/.antigen
   rm -f ~/.zshrc
-  ln -s $DOTRC/zshrc ~/.zshrc
-  ln -s $DOTRC/oh-my-zsh/custom/dklight.zsh ~/.oh-my-zsh/custom/
+  ln -s $DOTRC/_zshrc ~/.zshrc
 
   # Install needed fonts
-  mkdir -p $PROJECTS/fonts
-  cd $PROJECTS/fonts
+  mkdir fonts
+  cd fonts
   git clone https://github.com/Lokaltog/powerline-fonts.git
-  cd $PROJECTS/fonts/powerline-fonts
+  cd powerline-fonts
   ./install.sh
 
   # Make ZSH the default shell
@@ -33,13 +30,13 @@ fi
 
 # Install GNU Screen
 sudo apt-get install -y screen
-ln -s %DOTRC/screenrc ~/.screenrc
+ln -s $DOTRC/_screenrc ~/.screenrc
 
 # Install VIM
 sudo apt-get install -y vim-gtk
 rm -f ~/.vimrc
-ln -s $DOTRC/vimrc ~/.vimrc
-mkdir -p ~/.vim/autoload ~/.vim/bundle
+ln -s $DOTRC/_vimrc ~/.vimrc
+mkdir -p ~/.vim/bundle
 
 # Use Vundle to manage plugins
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
